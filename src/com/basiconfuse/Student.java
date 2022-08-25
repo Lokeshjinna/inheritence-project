a313a0ffb8452191ff514a101a42f69ee24c1677
jinna lokesh <lokeshjinna4@gamail.com>
2022-08-25 21:51:09 +0530

    all files are added

---
+++ src/com/basiconfuse/Student.java
@@ -0,0 +1,29 @@
+package com.basiconfuse;
+
+public class Student {
+	
+String name;
+int rno;
+Student()
+{
+	name="abc";
+	rno=125;
+}
+Student(String str,int n)
+{
+name=str;
+rno=n;
+}
+
+	public static void main(String[] args) {
+{
+Student s1=new Student();
+Student s2=new Student("def",456);
+System.out.println(s1.name);
+System.out.println(s1.rno);
+System.out.println(s2.name);
+System.out.println(s2.rno);
+}
+	}
+
+}
